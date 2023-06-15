9afaf99bbbb2a2603fbfb96ec02af8927438c8fd
KhensaDPJ <59077006+KhensaDPJ@users.noreply.github.com>
2023-06-15 21:57:12 +0700

    edit err for app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Login from './login/Login'
+
 import Hero from './componets/Hero'
 import Navbar from './componets/Navbar'
 import Offers from './componets/Offers'
@@ -11,7 +8,6 @@ import ImageSlider from './componets/ImageSlider'
 import Footer from './componets/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
